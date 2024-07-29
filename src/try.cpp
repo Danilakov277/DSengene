c6bbaa76d509a4277a4a737cf1c3f8c289fc0517
Anzherus <125902900+Anzherus@users.noreply.github.com>
2024-07-29 06:46:20 +0300

    add basick function for OpenGl

--- src/try.cpp
+++ src/try.cpp
@@ -2,21 +2,61 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+//add call back functon
+void error_callback(int error, const char* description)
+{
+    std::cout << "Eror! " << description << std::endl;
+}
+
+int MonitorXsize = 640;
+int MonitorYsize = 480;
+
+//function to change size of app
+void glfwWindowSizeCallback(GLFWwindow* window, int X,int Y)
+{
+    MonitorXsize = X;
+    MonitorYsize = Y;
+    glViewport(0, 0, MonitorXsize, MonitorYsize);
+}
+
+//create key action function 
+void glfwKeyCallback(GLFWwindow* window, int key, int scanecode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
     GLFWwindow* window;
+     
+    //set call back function
+    glfwSetErrorCallback(error_callback);
 
     /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    if (!glfwInit())return -1;
+
+    //add version controler of opengl and enabled crosplatform of openGl
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(MonitorXsize, MonitorYsize, "DSEngene", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+
+    //set function to change size of app
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+
+    //set key action function 
+    glfwSetKeyCallback(window, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -26,7 +66,10 @@ int main(void)
         std::cout << "Cant load GLAD" << std::endl;
         return -1;
     }
-    std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
+ 
+    //add output of information adbout local OpenGL
+    std::cout << "render: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGl Version: " << glGetString(GL_VERSION) << std::endl;
 
     glClearColor(0, 1, 0, 1);
 
